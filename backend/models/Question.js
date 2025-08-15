f0bb13871d6969016b50e9b8b2517dbf578fddc3
DURGA PRASAD GALLA <100194719+Durgaprasadgalla@users.noreply.github.com>
2025-08-15 10:27:52 +0530

    Delete backend directory

--- backend/models/Question.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const QuestionSchema = new mongoose.Schema({
-  question: { type: String, required: true },
-  options: { type: [String], required: true },
-  answer: { type: Number, required: true } // index of correct option
-});
-
-export default mongoose.model("Question", QuestionSchema);
