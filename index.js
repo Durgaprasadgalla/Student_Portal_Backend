8217ae4bd522d51daca3c717b947371c88dbe93b
DURGA PRASAD GALLA <100194719+Durgaprasadgalla@users.noreply.github.com>
2025-08-15 10:53:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+// Middleware
 app.use(express.json());
+app.use(cors({
+  origin: "https://Durgaprasadgalla.github.io",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
 // DB connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error("MongoDB connection error:", err));
-
-
-
-app.use(cors({
-  origin: "https://Durgaprasadgalla.github.io"
-}));
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -33,5 +32,3 @@ app.use("/api/exam", examRoutes);
 app.get("/", (req, res) => res.send("API is running..."));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
