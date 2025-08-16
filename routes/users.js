e8d337e06cf4442ea2352925edd8b0b21144af35
DURGA PRASAD GALLA <100194719+Durgaprasadgalla@users.noreply.github.com>
2025-08-16 18:19:48 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,26 @@
 import express from "express";
-import User from "../models/User.js"; // import your User model
+import User from "../models/User.js";
 
 export const router = express.Router();
 
-// GET all users
+//  GET all users
 router.get("/", async (req, res) => {
   try {
-    const users = await User.find(); // fetch all users from MongoDB
-    res.json(users);
+    const users = await User.find(); // fetch all users
+    res.json(users); // send response
   } catch (err) {
     res.status(500).json({ message: err.message });
+  }
+});
+
+//  POST new user (optional, to add users from Postman)
+router.post("/", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const newUser = new User({ username, password });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
